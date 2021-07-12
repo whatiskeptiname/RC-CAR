c8087731a1f6ad6bce47f936f6fb5ea9e3619eea
susang <susangmree@gmail.com>
2021-07-12 22:27:28 +0545

    first commit

---
+++ susu_ppm.ino
@@ -0,0 +1,26 @@
+#include"susu_wrapper.h"
+
+void loop()
+{
+  readPPMData();
+  for (int i = 0; i < 8; i++)
+  {
+    Serial.print(" " + String(ch[i]) + " ");
+  }
+  Serial.println();
+  delay(1000);
+  return;
+  if (system_alive())
+  {
+    servo_posn();
+    servo_dance();
+    get_gear();
+    mobilize();
+  }
+  else
+  {
+    neutral();
+    Serial.println("System on hold");
+  }
+  Serial.println();
+}
